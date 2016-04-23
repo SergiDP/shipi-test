e9a5d54b6fdef8173fefccf35b1f99e9a7552fa7
Sergi Delgado <sergi.delgadopuig@gmail.com>
2016-04-23 20:23:10 +0200

    change users text

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('hello world');
+  res.send('Hola Mundo');
 });
 
 module.exports = router;
